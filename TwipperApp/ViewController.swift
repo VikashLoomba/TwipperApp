c56e8f898b99b660d2197adebe0f670db1ee6ae0
Vikash Loomba <loomba.vikash@gmail.com>
2015-09-07 16:06:59 -0700

    Initial Commit

---
+++ TwipperApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwipperApp
+//
+//  Created by Vikash Loomba on 9/7/15.
+//  Copyright © 2015 Vikash Loomba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
